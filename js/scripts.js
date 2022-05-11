8686869c56c5897431ff0f5c57bc37880c0795b2
dtc16hd4801030001 <dtc16hd4801030001@ictu.edu.vn>
2022-05-11 19:50:10 +0700

    done

---
+++ js/scripts.js
@@ -0,0 +1,42 @@
+/*!
+* Start Bootstrap - Resume v7.0.5 (https://startbootstrap.com/theme/resume)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+document.addEventListener('DOMContentLoaded' , () => {
+    const content = document.getElementById('showhide-resume');
+    const getIdEdu = document.getElementById('btn-edu');
+    getIdEdu.addEventListener('click', () => {
+        content.classList.toggle('hidden-resume');
+        getIdEdu.classList.toggle('active');
+    })
+})
